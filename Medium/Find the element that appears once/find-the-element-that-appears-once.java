6d049049e3b11c19e55025471609f97a2d016bc3
AADRIKA SINGH <91376920+aadrikasingh18@users.noreply.github.com>
2023-04-15 12:02:37 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the element that appears once/find-the-element-that-appears-once.java
+++ Medium/Find the element that appears once/find-the-element-that-appears-once.java
@@ -31,31 +31,13 @@ class GfG
 
 class Sol
 {
-    public static int search(int nums[], int n) 
+    public static int search(int a[], int n) 
     {
-          int t = 0;
-          
-          if(n == 1)
-              return nums[0];
-          else
-          {
-              Arrays.sort(nums);
-              if(nums[0] != nums[1])
-                return nums[0];
-              else if(nums[n-2] != nums[n-1])
-                return nums[n-1];
-              else
-              {
-                for(int i=1; i<n-2; i++)
-                {
-                        if((nums[i-1] != nums[i]) && (nums[i] != nums[i+1]))
-                        {
-                                t = nums[i];
-                                break;
-                        }
-                 }
-              }
-          }
-            return t;
+        int xor = 0;
+        for(int i=0; i<n; i++)
+        {
+            xor = a[i] ^ xor;
+        }
+        return xor;
     }
 }
