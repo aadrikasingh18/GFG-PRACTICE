a34bbf485a2fa8e3abbe579e37fa4a65b0365991
AADRIKA SINGH <91376920+aadrikasingh18@users.noreply.github.com>
2023-05-31 23:46:25 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find Number of Numbers/find-number-of-numbers.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+class Numbers{
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t-->0)
+	{
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i = 0; i < n; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int k=sc.nextInt();
+		GfG g=new GfG();
+		System.out.println(g.num(a,n,k));
+	}
+}
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG
+{
+          public static int num(int a[], int n, int k)
+            {
+                 //Your code here
+                 int count = 0;
+                 for(int i=0; i<n; i++)
+                 {
+                     int key = a[i];
+                     while(key != 0)
+                     {
+                         int r = key % 10;
+                         key = key / 10;
+                         if(r == k)
+                         count++;
+                     }
+                 }
+                 return count;
+            }
+}
