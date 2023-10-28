76543fd70a2431709acd2dc499fd493410f5b279
AADRIKA SINGH <91376920+aadrikasingh18@users.noreply.github.com>
2023-10-29 01:18:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rotate a Linked List/rotate-a-linked-list.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public Node rotate(Node head, int k) 
+    {
+        Node temp=head;
+        while(temp.next!=null)
+        {       
+            temp=temp.next;
+        }
+        temp.next=head;
+        Node curr=head;
+        while(k!=1)
+        {                           // iterate until k value 
+            curr=curr.next;
+            k--;
+        }
+        Node node=curr.next;          //save k next  value because it is                                                                    head after rotation
+        curr.next=null;                       //make k next value=null;
+        return node;
+    }
+}
