f2d9a4de680079d247ac7215c1a832506c64abab
AADRIKA SINGH <91376920+aadrikasingh18@users.noreply.github.com>
2023-11-11 20:42:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove every k'th node/remove-every-kth-node.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+	Node next;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+class Delete_Kth_Node
+{
+	Node head;	
+	
+	void addToTheLast(Node node)
+	{
+		if(head == null)
+		head = node;
+		else
+		{
+			Node temp = head;
+			while(temp.next != null)
+			temp = temp.next;
+			
+			temp.next = node;
+		} }	
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Delete_Kth_Node list = new Delete_Kth_Node();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            list.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				list.addToTheLast(new Node(a));
+			}
+			int k = sc.nextInt();					  
+			//System.out.println(list.head.data+" "+ k);
+			list.head = new GfG().delete(list.head,k); 
+			Node temp = list.head;
+			while(temp!=null)
+			{
+				System.out.print(temp.data+ " ");
+			    temp = temp.next;
+			}
+			System.out.println();
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Link list Node
+class Node
+{
+	Node next;
+	int data;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class GfG
+{
+    Node delete(Node head, int k)
+    {
+        if(k == 0)
+        return head;
+        
+        if(k == 1)
+        return null;
+        
+        int c = 1;
+        Node curr = head;
+        while(curr != null)
+        {
+            if(c == k-1 && curr.next != null)
+            {
+                curr.next = curr.next.next;
+                c = 0;
+            }
+            c++;
+            curr = curr.next;
+        }
+        return head;
+    }
+}
